29c63c62145539a08a83c42da381e5cd60405680
José Miguel <clasiccode@gmail.com>
2023-08-31 14:12:37 +0200

    Commit inicial Clase 1

---
+++ Clase 1 - Diseño de la Arquitectura de la API/Centurion/API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public List<string> Get()
+    {
+        return new List<string>();
+    }
+}
+
